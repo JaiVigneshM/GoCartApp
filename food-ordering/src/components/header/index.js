bc4aef5a9f6070514b83e8945f20303bea117945
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2021-06-05 12:35:55 +0530

    create and activate header

---
+++ food-ordering/src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(props) {
+
+    return (<header>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>)
+}
